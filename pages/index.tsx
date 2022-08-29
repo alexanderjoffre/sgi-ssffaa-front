80e72006f026a3247df00b03f395beaf1d385449
Alexander Joffre Aguirre <alexanderjoffreaguirre@Alexanders-Mac-mini.local>
2022-08-29 16:26:25 -0500

    chore: sass main features added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import { Layout } from '../src/layouts/Public';
 
 const Home: NextPage = () => {
   return (
-    <></>
-  )
+    <Layout title='Inicio'>
+      <h1>Hola</h1>
+    </Layout>
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/layouts/Public.tsx
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+import { useEffect } from 'react';
+
+interface ILayoutProps {
+
+	// page keywords meta tag content
+	kw?: string;
+
+	// page description meta tag content
+	desc?: string;
+
+	// true if page must be indexed by search engines
+	noIndex?: boolean;
+
+	// page title
+	title: string;
+
+	// page content
+	children: React.ReactNode;
+
+}
+
+export const Layout = (props: ILayoutProps) => {
+	return (
+		<>
+			<Head>
+				<meta name="keywords" content={props.kw} />
+				<meta name="description" content={props.desc} />
+				<meta name="robots" content={ props.noIndex ? 'noindex, nofollow' : 'index, follow' } />
+				<title>{process.env.NEXT_PUBLIC_APP_NAME} | {props.title}</title>
+			</Head>
+			<main>
+				{props.children}
+			</main>
+			<footer>
+				<span>Sistema de Gestión Institucional v{process.env.NEXT_PUBLIC_APP_VERSION}</span>
+			</footer>
+		</>
+	);
+}
